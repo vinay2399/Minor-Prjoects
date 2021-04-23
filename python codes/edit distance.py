0faca778fa969a3e740632566b182e5f952ca9a7
Vinay Sharma <vinaysharma9923@gmail.com>
2021-04-23 19:21:21 +0530

    edit distance problem via naive recursion

---
+++ python codes/edit distance.py
@@ -0,0 +1,17 @@
+def editDistance(str1, str2, m, n):
+	if m == 0:
+		return n
+
+	if n == 0:
+		return m
+
+	if str1[m-1] == str2[n-1]:
+		return editDistance(str1, str2, m-1, n-1)
+
+	return 1 + min(editDistance(str1, str2, m, n-1),
+				editDistance(str1, str2, m-1, n), 
+				editDistance(str1, str2, m-1, n-1)
+				)
+str1 = "abcd"
+str2 = "abfde"
+print(editDistance(str1, str2, len(str1), len(str2)))
